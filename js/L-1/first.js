5d0a164eb365609ce6f500c8b2e01978dff7d82a
anurag-chandra9 <anuragkrstm01@gmail.com>
2024-08-26 11:44:00 +0530

    first commit

---
+++ js/L-1/first.js
@@ -0,0 +1,27 @@
+
+// fullName="anurag chandra ";
+// console.log(fullName);
+// x=null; 
+// y=undefined;
+// isfollow=false;
+// console.log(x); 
+// console.log(isfollow);
+// let fullname ="anurag chandra ";
+// console.log(fullname);
+// let age=24;
+// const product={
+//     title: "ball pen",
+//     rating: 4,
+//     offer:5,
+//     price:270,
+
+// };
+// console.log(product);
+const profile= {
+    username:"@anuragchandra9",
+    isFollow: false,
+    followers: 123,
+    following: 21,
+
+};
+console.log(profile);
